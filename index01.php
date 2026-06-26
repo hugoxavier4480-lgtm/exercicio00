21fd16ce9bb8fcca3618fb65c70550d3da8263d3
hugoxavier4480-lgtm <hugoxavier4480@gmail.com>
2026-06-26 09:58:32 +0100

    tydfyjgg

---
+++ index01.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-pt">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php
+    $texto = "Só HTML - Escrito em PHP";
+    echo $texto;
+    ?></title>
+</head>
+
+<body>
+    <h1>
+    <?php
+    echo 'Olá'?>-<?php echo 'Mundo!'
+    ?>
+    </h1>
+    <?= '<p>Este é um parágrafo de texto.</p>' ?>
+</body>
+
+</html>
---
+++ index02.php
@@ -0,0 +1,17 @@
+<?php
+'<!doctype html>
+<html lang="pt-pt">
+  <head>
+    <meta charset="UTF-8" />
+    <title>Só HTML</title>
+  </head>
+  <body>
+    <h1>
+      Olá mundo
+    </h1>
+    <p>
+      Este é um parágrafo de texto.
+    </p>
+  </body>
+</html>';
+?>
